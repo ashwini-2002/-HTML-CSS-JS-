0153ac3ce953a4cfac98b2923070edf4ee197b77
Ashwini CM <ashwini200213@gmail.com>
2026-01-27 11:31:06 +0530

    Add files via upload

---
+++ conditions,loop/index.js
@@ -0,0 +1,70 @@
+// let age=17;
+// if(age>=18){
+//     console.log("eligible to vote");
+// }
+
+
+// if(age>=18){
+//     console.log("eligible to vote");
+// }else{
+//     console.log("not eligible to vote");
+// }
+
+// let babyAge=3;
+// if(babyAge ===6){
+//     console.log("Baby can join LKG")
+// }
+// else if (babyAge ===7){
+//     console.log("Baby can join UKG");
+// }else{
+//     console.log("baby can't join the school");
+// }
+
+// babyAge ===6 ? console.log("Baby can join LKG"):console.log("Baby can join UKG");
+
+
+// let time=12;
+// switch(time){
+//     case 6:
+//         console.log("good morning");
+
+//     case 12:
+//         console.log("good Afternoon");
+
+//     default:
+//         console.log("invalid time");
+//         break;
+        
+//     }
+
+
+
+
+    //loops
+
+
+//for
+
+let fruits=["apple","grapes","orange","mango"];
+for(let i=0;i<fruits.length;i++){
+console.log(fruits[i]);
+}
+
+//while
+
+// let i=0;
+// let n=5;
+// while (i<n){
+//     console.log(i);
+//     i++;
+// }
+  //do while
+
+//   let join=0;
+//   let m=5; 
+//   do{
+//     console.log(i);
+//     i++;   
+//   } while(j<=m);
+
+let b=fruits.map((item,index) => console.log(item));
