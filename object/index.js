efd8b28b1c7b94948aec088b54dea89e30b5e796
Ashwini CM <ashwini200213@gmail.com>
2026-01-27 11:44:25 +0530

    Add files via upload

---
+++ object/index.js
@@ -0,0 +1,45 @@
+// const stu={
+//     Sname:"ash",
+//     Sid:123,
+//     Semai:"ashi@123"
+// }
+// console.log(stu);
+// stu.Spin=6576576
+// console.log(stu);
+
+// let emp=new Object()
+// emp.ename="ashhhh",
+// emp.eid=222,
+// console.log(emp);
+
+// let arr= new Array(1,2,3,4)
+// console.log(arr);
+
+// function movies(name,actor){
+// this.name=name;
+// this.actor=actor;
+
+// }
+// let movie1=new movies("devara","NTR")
+// console.log(movie1);
+
+
+
+
+
+object.key ()
+
+let emp={
+    ename:"ashwini",
+    eAge:23,
+    esal:30000,
+    eloc:"india",
+};
+emp.eName ="ASHI";
+console.log(emp)
+emp.epin=56001;
+console.log(Object.keys(emp));
+console.log(Object.values(emp));
+console.log(Object.entries(emp));
+
+
