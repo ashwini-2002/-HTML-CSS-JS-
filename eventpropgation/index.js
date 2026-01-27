a6665cb47db23c8fd8625e848895c1716aadeef4
Ashwini CM <ashwini200213@gmail.com>
2026-01-27 11:42:28 +0530

    Add files via upload

---
+++ eventpropgation/index.js
@@ -0,0 +1,12 @@
+let grandpa=document.getElementById("grandparent");
+grandpa.addEventListener("click",function(){
+grandpa.style.backgroundColor="red";
+});
+let papa=document.getElementById("parent");
+papa.addEventListener("click",function(){
+papa.style.backgroundColor="yellow";
+});
+let child=document.getElementById("child");
+child.addEventListener("click",function(){
+child.style.backgroundColor="blue";
+});
