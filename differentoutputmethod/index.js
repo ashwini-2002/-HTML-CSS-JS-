a9b7e18c8d24f705049cfa7d1a2d2bd56536e657
Ashwini CM <ashwini200213@gmail.com>
2026-01-27 11:41:22 +0530

    Add files via upload

---
+++ differentoutputmethod/index.js
@@ -0,0 +1,134 @@
+// console.log("hello");
+// console.warn("i am warning");
+// console.error("i am error");
+// alert("i am alert");
+// confirm("i am confirm");
+// document.writeln(" i am ashwini")
+// Document.writeln("i am developer")
+
+// "use strict";     
+
+// x="ironman";  
+// console.log(x);
+
+// let a="hello";
+// console.log(a);
+
+// console.log(typeof a);
+
+// let b="I'm ashwini";
+// console.log(b);
+
+// let c='i am "ashwini';
+// console.log(c);
+
+// let d=`I'm "Aswhini"`;
+// console.log(d);
+
+// let e=`i am ashwini
+// i'm from bangalore`;
+// console.log(e);
+
+//string
+ let a="Good Morning";
+
+let b=a.length;
+console.log(b);
+
+let c=a.charAt(5);
+console.log(c);
+
+let d=a.charCodeAt(0);
+console.log(d);
+
+let e=a.toUpperCase();
+console.log(e);
+
+let f=a.toLowerCase();
+console.log(f);
+
+let g=a.startsWith("good");
+console.log(g);
+
+
+let h=a.endsWith("g");
+console.log(h);
+
+
+let i=a.includes("M");
+console.log(i);
+
+
+let j=a.concat("everyone");
+console.log(j);
+
+
+let k=a.indexOf(10);
+console.log(1);
+
+let l=a.repeat(10);
+console.log(l);
+
+let m=a.replace("morning","night");
+console.log(m);
+
+let n=a.slice(0,4);
+console.log(n);
+
+let o=a.split("");
+let p=a.split(" ");
+let q=a.split("o");
+let r=a.split(o)
+console.log(o);
+console.log(p);
+console.log(q);
+console.log(r);
+
+let x=prompt("enter ur name");
+let y=x.charAt(0).toLowerCase().concat(x.slice(1));
+console.log(y);
+
+
+
+//numbers
+// let a=10;
+// console.log(a);
+// console.log(typeof a);
+
+// let b=20.5;
+// console.log(b);
+// console.log(typeof b);
+
+//number methods
+// let pi=3.1423521524;
+// let x=pi.toFixed(2);
+// console.log(x);
+// console.log(typeof x);
+
+// let z=3.45125;
+// let zz =z.toString();
+// console.log(zz, typeof zz);
+
+// let a=parseInt(prompt("enter your age"));
+// console.log(a,typeof a);
+
+
+//boolean
+// let a=true;
+// console.log(a);
+// console.log(typeof a);
+
+// let b=false;
+// console.log(b);
+// console.log(typeof b);
+
+
+// let a=undefined;
+// console.log(a);
+// console.log(typeof a);
+
+// let a=null;
+// console.log(a);
+// console.log( typeof a);
+
+
