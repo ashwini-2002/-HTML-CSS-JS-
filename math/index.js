10f4b43eb3e25d7baf4fb97e13573976c8a4e411
Ashwini CM <ashwini200213@gmail.com>
2026-01-27 11:44:09 +0530

    Add files via upload

---
+++ math/index.js
@@ -0,0 +1,25 @@
+let a = Math.random();
+console.log(a);
+
+let b=Math.round(3.4);
+console.log(b);
+
+let c=Math.ceil(3.4);            //ceil=consider the highest value
+console.log(c);
+
+let d=Math.floor(5.9);           //consider the lowest value
+console.log(d);
+
+let e=Math.pow(2,3);           //2^3=8
+console.log(e);
+
+let f=Math.sqrt(225);        
+console.log(f);
+
+let g=Math.max(1,2,3,4,5);
+console.log(g);
+
+let h=Math.min(1,2,3,4,5);
+console.log(h);
+
+
