d332c4a075f2e26496fc91f9f71dabfe0ee87144
Ashwini CM <ashwini200213@gmail.com>
2026-01-27 11:31:55 +0530

    Add files via upload

---
+++ date/index.js
@@ -0,0 +1,20 @@
+let a=new Date();
+console.log(a);
+
+let b= a.getDate();
+console.log(b);
+
+let c=a.getFullYear();
+console.log(c);
+
+let d=a.getHours();
+console.log(d);
+
+let e=a.getMonth();
+console.log(e);
+
+let f=a.getMinutes();
+console.log(f);
+
+let g=a.getSeconds();
+console.log(g);
